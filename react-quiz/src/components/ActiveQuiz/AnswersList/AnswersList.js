06d029de217117bd5401f2431d225f72856e6a48
andreiha <z.karolin@gmail.com>
2021-05-01 16:14:15 +0300

    feat: basic layout

---
+++ react-quiz/src/components/ActiveQuiz/AnswersList/AnswersList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AnswersList.css';
+import AnswerItem from './AnswerItem/AnswerItem';
+
+const AnswersList = (props) => (
+    <ul className='AnswersList'>
+        {props.answers.map((answer, index) => {
+            return <AnswerItem key={index} answer={answer} />;
+        })}
+    </ul>
+);
+export default AnswersList;
